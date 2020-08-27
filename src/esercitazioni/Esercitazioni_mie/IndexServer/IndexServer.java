ff4ec3d6f18917a9f3c27b3198bd331883d88819
Giuseppe Mattia Greco <giumatt99@gmail.com>
2020-08-27 11:37:30 +0200

    5

--- src/esercitazioni/Esercitazioni_mie/IndexServer/IndexServer.java
+++ src/esercitazioni/Esercitazioni_mie/IndexServer/IndexServer.java
@@ -1,9 +1,11 @@
 package Esercitazioni_mie.IndexServer;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
-import java.util.*;
-
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 public class IndexServer {
     private Map<File, InetAddress> data;
     private final int uPort = 3000;
@@ -48,6 +50,56 @@ public class IndexServer {
                 } catch (IOException e)  { e.printStackTrace(); }
             }
         }
+    }
+
+    class GestisciClient extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Attesa nuova ricerca...");
+                    Socket tSocket = server.accept();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(tSocket.getInputStream()));
+
+                    String msg = br.readLine();
+                    System.out.println("Ricevi la ricerca: " + msg);
+
+                    String[] parti = msg.split("#");
+                    String[] keywords = parti[1].split(",");
+
+                    System.out.format("Ricevo la ricerca con filename %s e chiavi %s%n", parti[0], parti[1]);
+
+                    String ret = "";
+                    synchronized (data) {
+                        for (Entry<File, InetAddress> entry: data.entrySet()) {
+                            File file = entry.getKey();
+
+                            if (file.getFilename().equals(parti[0])) {
+                                boolean trovato = true;
+                                for (int i = 0; i < keywords.length; i++) {
+                                    trovato = false;
+                                    for (int j = 0; j < file.getKeywords().length; j++) {
+                                        if (keywords[i].equals(file.getKeywords()[j])) {
+                                            trovato = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (trovato) ret += entry.getValue().toString();
+                            }
+                        }
+                    }
+                    System.out.println("Invio la risposta " + ret);
+                    PrintWriter pw = new PrintWriter(tSocket.getOutputStream(), true);
+                    pw.println(ret);
+                    br.close(); pw.close(); tSocket.close();
+                } catch (IOException e) { e.printStackTrace(); }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        IndexServer idexS = new IndexServer();
     }
 }
 
