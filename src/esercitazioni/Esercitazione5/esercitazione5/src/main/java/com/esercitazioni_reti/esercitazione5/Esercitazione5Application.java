b19cc403243e6537fa2b9e3a2de7d84d16a650e0
Giuseppe Mattia Greco <giumatt99@gmail.com>
2020-08-31 16:16:14 +0200

    4

---
+++ src/esercitazioni/Esercitazione5/esercitazione5/src/main/java/com/esercitazioni_reti/esercitazione5/Esercitazione5Application.java
@@ -0,0 +1,13 @@
+package com.esercitazioni_reti.esercitazione5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione5Application.class, args);
+	}
+
+}
