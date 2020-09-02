da198ad740325391dad98293b1457a24f37fc10c
Giuseppe Mattia Greco <giumatt99@gmail.com>
2020-09-02 17:14:42 +0200

    8

--- src/esercitazioni/Esercitazioni_mie/appellonovembre2019/Client.java
+++ src/esercitazioni/Esercitazioni_mie/appellonovembre2019/Client.java
@@ -1,5 +1,19 @@
 package Esercitazioni_mie.appellonovembre2019;
 
+import java.io.ObjectOutputStream;
+import java.net.*;
+
 public class Client extends Thread {
-    
+    private final static int tPort = 1111;
+
+    public static void main(String[] args) {
+        try {
+            Richiesta richiesta = new Richiesta("Smartphone XYZ", 5, 150);
+            Socket tSocket = new Socket("server.dimes.it", tPort);
+            ObjectOutputStream oos = new ObjectOutputStream(tSocket.getOutputStream());
+
+            oos.writeObject(richiesta);
+            oos.flush(); tSocket.close();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
 }
--- src/esercitazioni/Esercitazioni_mie/appellonovembre2019/Negozio.java
+++ src/esercitazioni/Esercitazioni_mie/appellonovembre2019/Negozio.java
@@ -1,5 +1,7 @@
 package Esercitazioni_mie.appellonovembre2019;
 
 public class Negozio {
-    
+    public static void main(String[] args) {
+        
+    }
 }
